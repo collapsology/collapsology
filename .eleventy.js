b6c9f6d792afa785262a8e0d09d65f925af15ff4
Jérôme Coupé <jercoupe@gmail.com>
2020-08-30 21:22:59 +0200

    no shuffle on books for homepages FR and EN

--- .eleventy.js
+++ .eleventy.js
@@ -1,32 +1,4 @@
 const lodash = require("lodash");
-const { data } = require("autoprefixer");
-
-/**
- * Get all unique key values from a collection
- *
- * @param {Array} collectionArray - collection to loop through
- * @param {String} key - key to get values from
- */
-function getAllKeyValues(collectionArray, key) {
-  // get all values from collection
-  let allValues = collectionArray.map((item) => {
-    let values = item.data[key] ? item.data[key] : [];
-    return values;
-  });
-
-  // flatten values array
-  allValues = lodash.flattenDeep(allValues);
-  // to lowercase
-  allValues = allValues.map((item) => item.toLowerCase());
-  // remove duplicates
-  allValues = [...new Set(allValues)];
-  // order alphabetically
-  allValues = allValues.sort(function (a, b) {
-    return a.localeCompare(b, "en", { sensitivity: "base" });
-  });
-  // return
-  return allValues;
-}
 
 module.exports = function (eleventyConfig) {
   // filters
