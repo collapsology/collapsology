09ee9483b6c5e4ea55ee4e3991acbb4bfcc1465a
raphaelstevens <69911393+raphaelstevens@users.noreply.github.com>
2020-08-31 12:21:27 +0200

    Update mainnav.js

--- src/_data/mainnav.js
+++ src/_data/mainnav.js
@@ -6,7 +6,7 @@ module.exports = [
       link: "/fr/science/"
     },
     en: {
-      label: "Scientific litterature",
+      label: "Scientific literature",
       link: "/en/science/"
     }
   },
