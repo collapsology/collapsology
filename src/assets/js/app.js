5191b93967721ef60749c41f764418b68e6dff8f
Jérôme Coupé <jercoupe@gmail.com>
2021-09-22 08:01:01 +0200

    feature; Switch to petite-vue and update dependencies

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,86 @@
-import Alpine from "alpinejs";
-import zotero from "./alpine/zotero.js";
+import axios from "axios";
+import { createApp } from "petite-vue";
 
-Alpine.data("zotero", zotero);
+const ITEMS_PER_PAGE = 10;
 
-Alpine.start();
+createApp({
+  records: [],
+  tags: [],
+  loading: true,
+  filtersOpen: false,
+  totalRecords: 0,
+  currentPage: 1,
+  totalPages: 0,
+  currentTag: "",
+  needle: "", // reactive (v-model)
+  searchQuery: "", // explicitly set
+  /**
+   * Fetch records from proxy
+   * - fetchRecords() is called with v-effect
+   *   so it runs everytime a variable it uses (reads)
+   *   is changed (currentPage, searchQuery, currentTag)
+   */
+  async fetchRecords() {
+    try {
+      // set loading
+      this.loading = true;
+
+      // hit proxy to get data
+      const response = await axios.get("/.netlify/functions/records", {
+        params: {
+          limit: ITEMS_PER_PAGE,
+          start: (this.currentPage - 1) * ITEMS_PER_PAGE,
+          q: this.searchQuery,
+          tag: this.currentTag,
+        },
+      });
+
+      // assign data and total
+      let { data, total } = response.data;
+
+      // set state
+      this.records = data;
+      this.totalRecords = total;
+      this.totalPages = Math.ceil(total / ITEMS_PER_PAGE);
+      this.loading = false;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  async fetchTags() {
+    try {
+      let { data } = await axios.get("/.netlify/functions/tags");
+      this.tags = data;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  setSearch() {
+    this.searchQuery = this.needle;
+  },
+  clearSearch() {
+    this.searchQuery = "";
+  },
+  setTag(id) {
+    this.currentTag = id;
+  },
+  clearTag() {
+    this.currentTag = "";
+  },
+  prevPage() {
+    if (this.currentPage === 1) return;
+    this.currentPage--;
+  },
+  nextPage() {
+    if (this.currentPage === this.totalPages) return;
+    this.currentPage++;
+  },
+  firstPage() {
+    if (this.currentPage === 1) return;
+    this.currentPage = 1;
+  },
+  lastPage() {
+    if (this.currentPage === this.totalPages) return;
+    this.currentPage = this.totalPages;
+  },
+}).mount("#app");
