194e1ed4ae2873762facc4308917e9332c8bd3fd
Jérôme Coupé <jercoupe@gmail.com>
2022-03-28 15:44:29 +0200

    config: requires on top instead of inline

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,39 @@
+// filters
+const dateISO = require("./src/_11ty/filters/date.js").dateISO;
+const dateFull = require("./src/_11ty/filters/date.js").dateFull;
+const dateFeed = require("./src/_11ty/filters/date.js").dateFeed;
+const dateYear = require("./src/_11ty/filters/date.js").dateYear;
+const limit = require("./src/_11ty/filters/limit.js");
+const translate = require("./src/_11ty/filters/translate.js");
+const shuffle = require("./src/_11ty/filters/shuffle.js");
+const setFileExtension = require("./src/_11ty/filters/setFileExtension.js");
+
+// collections
+const books_fr = require("./src/_11ty/collections/books_fr.js");
+const books_en = require("./src/_11ty/collections/books_en.js");
+const links_fr = require("./src/_11ty/collections/links_fr.js");
+const links_en = require("./src/_11ty/collections/links_en.js");
+const linksByTypes_fr = require("./src/_11ty/collections/linksByTypes_fr.js");
+const linksByTypes_en = require("./src/_11ty/collections/linksByTypes_en.js");
+
 module.exports = function (eleventyConfig) {
   // filters
-  eleventyConfig.addFilter("dateISO", require("./src/_11ty/filters/date.js").dateISO);
-  eleventyConfig.addFilter("dateFull", require("./src/_11ty/filters/date.js").dateFull);
-  eleventyConfig.addFilter("dateFeed", require("./src/_11ty/filters/date.js").dateFeed);
-  eleventyConfig.addFilter("dateYear", require("./src/_11ty/filters/date.js").dateYear);
-  eleventyConfig.addFilter("limit", require("./src/_11ty/filters/limit.js"));
-  eleventyConfig.addFilter("translate", require("./src/_11ty/filters/translate.js"));
-  eleventyConfig.addFilter("shuffle", require("./src/_11ty/filters/shuffle.js"));
-  eleventyConfig.addFilter("setFileExtension", require("./src/_11ty/filters/setFileExtension.js"));
+  eleventyConfig.addFilter("dateISO", dateISO);
+  eleventyConfig.addFilter("dateFull", dateFull);
+  eleventyConfig.addFilter("dateFeed", dateFeed);
+  eleventyConfig.addFilter("dateYear", dateYear);
+  eleventyConfig.addFilter("limit", limit);
+  eleventyConfig.addFilter("translate", translate);
+  eleventyConfig.addFilter("shuffle", shuffle);
+  eleventyConfig.addFilter("setFileExtension", setFileExtension);
 
   // collections
-  eleventyConfig.addCollection("books_fr", require("./src/_11ty/collections/books_fr.js"));
-  eleventyConfig.addCollection("books_en", require("./src/_11ty/collections/books_en.js"));
-  eleventyConfig.addCollection("links_fr", require("./src/_11ty/collections/links_fr.js"));
-  eleventyConfig.addCollection("links_en", require("./src/_11ty/collections/links_en.js"));
-  eleventyConfig.addCollection("linksByTypes_fr", require("./src/_11ty/collections/linksByTypes_fr.js"));
-  eleventyConfig.addCollection("linksByTypes_en", require("./src/_11ty/collections/linksByTypes_en.js"));
+  eleventyConfig.addCollection("books_fr", books_fr);
+  eleventyConfig.addCollection("books_en", books_en);
+  eleventyConfig.addCollection("links_fr", links_fr);
+  eleventyConfig.addCollection("links_en", links_en);
+  eleventyConfig.addCollection("linksByTypes_fr", linksByTypes_fr);
+  eleventyConfig.addCollection("linksByTypes_en", linksByTypes_en);
 
   // passthrough copy
   eleventyConfig.addPassthroughCopy({"./src/static/":"/"});
